48d0ce80be62b3c32efced8ce4a565d0998f0555
khal2023 <khalidoham@gmail.com>
2025-03-06 14:09:43 +0000

    merged everything

--- app/models/Payment.scala
+++ app/models/Payment.scala
@@ -90,6 +90,7 @@ case class Payment(
 
 // Companion object for payment
 object Payment {
+
   implicit val timestampFormat: Format[java.sql.Timestamp] = new Format[java.sql.Timestamp] {
     def writes(ts: Timestamp): JsValue = {
       // Convert to Instant and format as an ISO-8601 string
@@ -101,14 +102,6 @@ object Payment {
 
   implicit val paymentFormat: OFormat[Payment] = Json.format[Payment]
 
-  implicit val timestampFormat: Format[java.sql.Timestamp] = new Format[java.sql.Timestamp] {
-    def writes(ts: Timestamp): JsValue = {
-      // Convert to Instant and format as an ISO-8601 string
-      val formatted = DateTimeFormatter.ISO_INSTANT.format(ts.toInstant)
-      JsString(formatted)
-    }
-    def reads(json: JsValue): JsResult[java.sql.Timestamp] = json.validate[Long].map(new java.sql.Timestamp(_))
-  }
 }
 
 // Payment table definition
